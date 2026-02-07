38a87200bfad1d53174374a9a733f628a946bb40
Fatima Qaisar <fatima79176985@gmail.com>
2026-02-08 01:10:47 +0500

    Starting DSA

--- OOP-Labs/Lab-1/DataTypes.java
+++ OOP-Labs/Lab-1/DataTypes.java
@@ -1,7 +1,7 @@
 public class DataTypes{
 
   public static void main (String a[]){
-       System.out.println("Basic types in java:");
+       System.out.println("Primitive data types in java:");
 
          long l=100;
          int i=10;
@@ -16,16 +16,16 @@ public class DataTypes{
          boolean b1=true; //false
 
 //These 8 types are called primitive data types of java
-// String is not included in primitive types. charaacter ki array bna ke usme store krna parega string bnane ke liye
+// String is not included in primitive types. character ki array bna ke usme store krna parega string bnane ke liye
 
 //3 different methods to print
 
 System.out.println(" Print string amd cursor moves to new line");
 System.out.print("Print and cursor remains on the same line");
-System.out.printf("\nInformation %d",20 );             //%d specifies doubleya format? f will be written if float boolean hoga to b search this. \n is for new line
+System.out.printf("\nInformation %d",20 );             //%d-> integer, %f-> floating point, %b-> boolean, \n or %n is for new line, %.2f-> 2 decimal places
 //System.out.err for errors
 //System.out.err("Error log");
-//formatted output bnani hai to print f... e.g you want it to be shown as a table
+//Use printf for formatted output... e.g you want it to be shown as a table
 
 System.out.printf("\nValue in int %d",i);
 System.out.printf("\nValue in long %d",l);
@@ -33,7 +33,6 @@ System.out.printf("\nValue in short %d",s);
 System.out.printf("\nValue in char %c",c);
 System.out.printf("\nValue in int %d",i);
 System.out.printf("\nValue in int %d",i);
-
 
 }
 }
